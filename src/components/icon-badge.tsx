3ac8e80d061a7d055a2f37db66fb48b8a7e34d3a
Iyanda Olamidotun <olamidotun225@gmail.com>
2024-03-09 18:08:47 +0100

    add: createInternship

--- src/components/icon-badge.tsx
+++ src/components/icon-badge.tsx
@@ -47,3 +47,13 @@ const iconVariants = cva("", {
 type BackgroundVariantsProps = VariantProps<typeof backgroundVariants>;
 type IconVariantsProps = VariantProps<typeof iconVariants>;
 
+interface IconBadgeProps extends BackgroundVariantsProps, IconVariantsProps 
+{
+  icon: LucideIcon;
+}
+
+export const IconBadge = ({
+    icon: Icon,
+    variant,
+    size,
+})
